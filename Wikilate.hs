638bb5d92a53bcb93574f60d15e1c1802e8b624b
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2012-01-25 20:05:39 +0100

    Modified cmdline parsing to use options record

--- Wikilate.hs
+++ Wikilate.hs
@@ -9,6 +9,59 @@ import Data.List (intersperse)
 import Text.Regex (mkRegex, splitRegex)
 import System.Environment (getArgs)
 import System.Console.GetOpt
+import Network.HTTP.Base (urlEncodeVars)
+
+
+-- Main function
+
+main = do
+	argv <- getArgs
+	(flags, args) <- parseCmdLineArgs argv
+	print (flags, args)
+
+
+-- Program options & command line
+
+data Options = Options
+	{ optSourceLang :: String
+	, optDestLangs :: [String]
+	}
+	deriving (Show)	-- for debugging
+
+defaultOptions = Options
+	{ optSourceLang = "en"
+	, optDestLangs = ["de", "fr", "es", "pl"]
+	}
+
+cmdLineFlags :: [OptDescr (Options -> Options)]
+cmdLineFlags =
+	[ Option ['s'] ["source"]
+	  (ReqArg (\f opts -> opts { optSourceLang = f }) "LANG")
+	  "language of the source phrase"
+	, Option ['d'] ["dest"]
+	  (ReqArg (\f opts -> opts { optDestLangs = splitBy "," f }) "LANG[,LANG[,...]]")
+	  "destination language(s)"
+	]
+
+parseCmdLineArgs :: [String] -> IO (Options, [String])
+parseCmdLineArgs args = 
+	case getOpt Permute cmdLineFlags args of
+		(opts, params, [])	-> return (foldl (flip id) defaultOptions opts, params)
+		(_, _, errorMsgs)	-> error $ concat ("\n":errorMsgs) ++ "\n" ++ usage
+	where
+		usage = usageInfo header cmdLineFlags
+		header = "Usage: wikilate [OPTION...] phrase"
+
+
+-- Actual logic
+
+wikipediaUrl :: String -> String -> String
+wikipediaUrl sourceLang phrase =
+	concat ["http://", sourceLang, ".wikipedia.org/w/api.php?", urlEncodeVars urlArgs]
+	where
+		urlArgs = [("action", "query"), ("prop", "langlinks"), ("titles", phrase)]
+
+
 
 
 -- Utilities
@@ -21,31 +74,3 @@ splitBy delim =
 		escapeChars = ('\\':) . intersperse '\\'
 
 
--- Command line arguments
-
-data CmdLineFlag = SourceLang String | DestLang [String]
-	deriving Show
-
-cmdLineFlags :: [OptDescr CmdLineFlag]
-cmdLineFlags =
-	[ Option ['s'] ["source"] (ReqArg SourceLang "LANG") "language of the source phrase"
-	, Option ['d'] ["dest"] (ReqArg (DestLang . splitBy ",") "LANG[,LANG[,...]]") "destination language(s)"
-	]
-
-
-parseCmdLineArgs :: [String] -> IO ([CmdLineFlag], [String])
-parseCmdLineArgs args = 
-	case getOpt Permute cmdLineFlags args of
-		(opts, params, [])	-> return (opts, params)
-		(_, _, errorMsgs)	-> ioError (userError (concat errorMsgs ++ usage))
-	where
-		usage = usageInfo header cmdLineFlags
-		header = "Usage: wikilate [OPTION...] phrase"
-
-
--- Main function
-
-main = do
-	argv <- getArgs
-	(flags, args) <- parseCmdLineArgs argv
-	print (flags, args)
