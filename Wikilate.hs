2d2839f3658854aed6062778cd3128593841e468
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-07-29 21:24:39 +0100

    Reformat the code into a little better style`

--- Wikilate.hs
+++ Wikilate.hs
@@ -10,14 +10,13 @@ module Main where
 
 import Prelude hiding (catch)
 
-import Control.Monad (when, mapM, liftM)
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (when, mapM, void)
 import Control.Exception (catch, IOException)
 import Data.Maybe (fromJust)
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Monoid
-import Data.Functor (fmap)
-import Text.Regex (mkRegex, splitRegex)
 import Text.JSON
 import System.Environment (getArgs)
 import System.Console.GetOpt
@@ -36,8 +35,7 @@ main = do
         error "No phrase specified."
     let phrase = intercalate " " args
 
-    let doFetch = fetchTranslations phrase opts >> return ()
-    catch doFetch handleError
+    catch (void $ fetchTranslations phrase opts) handleError
   where
     handleError :: IOException -> IO ()
     handleError e =
@@ -70,99 +68,112 @@ cmdLineFlags =
 parseCmdLineArgs :: [String] -> IO (Options, [String])
 parseCmdLineArgs args =
     case getOpt Permute cmdLineFlags args of
-        (opts, params, [])  -> return (foldl (flip id) defaultOptions opts, params)
-        (_, _, errorMsgs)   -> error $ concat ("\n":errorMsgs) ++ "\n" ++ usage
-    where
-        usage = usageInfo header cmdLineFlags
-        header = "Usage: wikilate [OPTION...] phrase"
+        (opts, params, []) ->
+            return (foldl (flip id) defaultOptions opts, params)
+        (_, _, errorMsgs) ->
+            error $ concat ("\n":errorMsgs) ++ "\n" ++ usage
+  where
+    usage = usageInfo header cmdLineFlags
+    header = "Usage: wikilate [OPTION...] phrase"
 
 
--- Data type to hold translations
-
+-- | Holds translations as a mapping of (language, text),
 newtype Translations = Translations [(String, String)]
                        deriving (Monoid, Eq)
 
+-- | Filter translations through a list of languages.
 (<&>) :: Translations -> [String] -> Translations
-(Translations al) <&> list = Translations $ filter ((`elem` list) . fst) al
+(Translations al) <&> list =
+    Translations $ filter ((`elem` list) . fst) al
 
 instance Show Translations where
     show (Translations transAL) =
         intercalate "\n" $ translationsLines
-        where translationsLines = map (\(lang, tr) -> lang ++ ": " ++ tr) transAL
+      where
+        translationsLines = map (\(lang, tr) -> lang ++ ": " ++ tr) transAL
 
 
--- Retrieving translations
-
+-- | Retrieves translations of given phrase.
 fetchTranslations :: String -> Options -> IO Translations
 fetchTranslations phrase Options{..} = do
     fetchTranslationsPart phrase Nothing
-    where
-        fetchTranslationsPart :: String -> Maybe String -> IO Translations
-        fetchTranslationsPart phrase continue = do
-            let url = wikipediaUrl optSourceLang phrase continue
-            let request = Request { rqURI = url, rqMethod = GET,
-                                    rqHeaders = [], rqBody = "" }
-            response <- simpleHTTP request
-            case response of
-                Left err -> error $ "Error connecting to Wikipedia: " ++ show err
-                Right rsp -> handleWikipediaResponse rsp
+  where
+    fetchTranslationsPart :: String -> Maybe String -> IO Translations
+    fetchTranslationsPart phrase continue = do
+        let url = wikipediaUrl optSourceLang phrase continue
+        let request = Request { rqURI = url
+                              , rqMethod = GET
+                              , rqHeaders = []
+                              , rqBody = ""
+                              }
+        response <- simpleHTTP request
+        case response of
+            Left err -> error $ "Error connecting to Wikipedia: " ++ show err
+            Right rsp -> handleWikipediaResponse rsp
 
-        handleWikipediaResponse rsp =
-            case rspCode rsp of
-                (2,_,_) -> do
-                    translations <- parseTranslations $ rspBody rsp
-                    let filtered = translations <&> optDestLangs
-                    when (filtered /= mempty) $
-                        putStrLn $ (show filtered)
-                    case parseQueryContinue $ rspBody rsp of
-                        Error _ -> return translations
-                        Ok qc -> do
-                            nextPart <- fetchTranslationsPart phrase (Just qc)
-                            return $ translations `mappend` nextPart
-                otherwise -> error $ "Invalid HTTP response code: " ++ show (rspCode rsp)
+    handleWikipediaResponse rsp =
+        case rspCode rsp of
+            (2,_,_) -> do
+                translations <- parseTranslations $ rspBody rsp
+                let filtered = translations <&> optDestLangs
+                when (filtered /= mempty) $
+                    putStrLn $ (show filtered)
+                case parseQueryContinue $ rspBody rsp of
+                    Error _ -> return translations
+                    Ok qc -> do
+                        nextPart <- fetchTranslationsPart phrase (Just qc)
+                        return $ translations <> nextPart
+            otherwise ->
+                error $ "Invalid HTTP response code: " ++ show (rspCode rsp)
 
-        parseQueryContinue :: String -> Result String
-        parseQueryContinue jsonString = do
-            json <- decode jsonString
-            queryContinue <- json ! "query-continue"
-            langlinks <- queryContinue ! "langlinks"
-            langlinks ! "llcontinue"
+    parseQueryContinue :: String -> Result String
+    parseQueryContinue jsonString = do
+        json <- decode jsonString
+        queryContinue <- json ! "query-continue"
+        langlinks <- queryContinue ! "langlinks"
+        langlinks ! "llcontinue"
 
 
+-- | Construct Wikipedia URL for given source language, phrase,
+-- and an optional continuation token.
 wikipediaUrl :: String -> String -> Maybe String -> URI
 wikipediaUrl sourceLang phrase continue =
     fromJust $ parseURI url
-    where
-        url = concat ["http://", sourceLang, ".wikipedia.org/w/api.php?", urlEncodeVars urlArgs]
-        urlArgs = [("action", "query"), ("prop", "langlinks"), ("format", "json"), ("titles", phrase)]
-                  ++ maybe [] (\c -> [("llcontinue", c)]) continue
+  where
+    url = concat ["https://"
+                 , sourceLang
+                 , ".wikipedia.org/w/api.php?"
+                 , urlEncodeVars urlArgs
+                 ]
+    urlArgs = [("action", "query")
+              , ("prop", "langlinks")
+              , ("format", "json")
+              , ("titles", phrase)
+              ] ++ maybe [] (\c -> [("llcontinue", c)]) continue
 
 
--- Parsing translations
-
+-- | Parse Wikipedia response into a list of translations.
 parseTranslations :: String -> IO Translations
 parseTranslations jsonString =
     case readWikipediaJson jsonString of
         Ok t -> return t
         Error msg -> fail msg
-    where
-        readWikipediaJson jsonString = do
-            json <- decode jsonString
-            query <- json ! "query"
-            pages <- query ! "pages"
-            let [(_, (JSObject page))] = fromJSObject pages -- the only child of "pages"
-            langlinks <- page ! "langlinks"
-            readJSON $ langlinks
-
+  where
+    readWikipediaJson jsonString = do
+        json <- decode jsonString
+        query <- json ! "query"
+        pages <- query ! "pages"
+        -- Get the only child of "pages"
+        let [(_, (JSObject page))] = fromJSObject pages
+        langlinks <- page ! "langlinks"
+        readJSON $ langlinks
 
 instance JSON Translations where
     readJSON (JSArray jsonTrans) =
-        liftM Translations $ mapM readJSONTranslation jsonTrans
-        where
-            readJSONTranslation (JSObject jt) = do
-                langId <- jt ! "lang"
-                translated <- jt ! "*"
-                return (langId, translated)
+        Translations <$> mapM readJSONTranslation jsonTrans
+      where
+        readJSONTranslation (JSObject jt) =
+            (,) <$> jt ! "lang" <*> jt ! "*"
     showJSON = undefined
 
 
